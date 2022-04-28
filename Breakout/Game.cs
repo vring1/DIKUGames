99aa3e47fff63d958a09587807cdbea6a122d38e
Valdemar <valdemarring1@gmail.com>
2022-04-28 13:52:57 +0200

    This is a commit
    Co-authored-by: Valdemar <valdemarring1@gmail.com>
    Co-authored-by: Lauritz <ralamadam@gmail.com>

---
+++ Breakout/Game.cs
@@ -0,0 +1,38 @@
+using DIKUArcade;
+using DIKUArcade.GUI;
+using DIKUArcade.Input;
+using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using DIKUArcade.Physics;
+using System.Security.Principal;
+using System.Collections.Generic;
+//using DIKUArcade.EventBus;
+using DIKUArcade.Events;
+using System;
+
+namespace Breakout {
+    public class Game : DIKUGame//, IGameEventProcessor //DIKUGame 
+    {
+
+        public Game(WindowArgs windowArgs) : base(windowArgs){
+            
+        }
+        public override void Update(){
+
+        }
+        public override void Render(){
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Breakout/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DIKUArcade.GUI;
+
+namespace Breakout {
+    class Program {
+        static void Main(string[] args) {
+            System.Console.WriteLine("0");
+            var windowArgs = new WindowArgs() { Title = "Breakout" };  
+            var game = new Game(windowArgs);
+            game.Run();
+           
+        }
+    }
+}
